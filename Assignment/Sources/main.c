7f97eb5bef41584de2df9cabf8623ca3585d9f26
William Belcher <william.belcher@my.jcu.edu.au>
2021-08-24 21:04:18 +1000

    Add my work so far. Reading IMU sensor fused quaternion values and converting to Euler angles

---
+++ Assignment/Sources/main.c
@@ -0,0 +1,102 @@
+/* ###################################################################
+ **     Filename    : main.c
+ **     Project     : Lab_03
+ **     Processor   : MK22FN512VDC12
+ **     Version     : Driver 01.01
+ **     Compiler    : GNU C Compiler
+ **     Date/Time   : 2021-08-13, 13:30, # CodeGen: 0
+ **     Abstract    :
+ **         Main module.
+ **         This module contains user's application code.
+ **     Settings    :
+ **     Contents    :
+ **         No public methods
+ **
+ ** ###################################################################*/
+/*!
+ ** @file main.c
+ ** @version 01.01
+ ** @brief
+ **         Main module.
+ **         This module contains user's application code.
+ */
+/*!
+ **  @addtogroup main_module main module documentation
+ **  @{
+ */
+/* MODULE main */
+
+
+/* Including needed modules to compile this module/procedure */
+#include "Cpu.h"
+#include "Events.h"
+#include "Pins1.h"
+#include "CI2C1.h"
+#include "IntI2cLdd1.h"
+/* Including shared modules, which are used for whole project */
+#include "PE_Types.h"
+#include "PE_Error.h"
+#include "PE_Const.h"
+#include "IO_Map.h"
+#include "PDD_Includes.h"
+#include "Init_Config.h"
+#include "BNO085.h"
+#include "Sensors.h"
+#include <stdbool.h>
+/* User includes (#include below this line is not maintained by Processor Expert) */
+#define BNO080_DEFAULT_ADDRESS 0x4A
+#define PI 3.14159
+/*lint -save  -e970 Disable MISRA rule (6.3) checking. */
+int main(void)
+/*lint -restore Enable MISRA rule (6.3) checking. */
+{
+	/* Write your local variable definition here */
+
+	/*** Processor Expert internal initialization. DON'T REMOVE THIS CODE!!! ***/
+	PE_low_level_init();
+	/*** End of Processor Expert internal initialization.                    ***/
+
+//	/* Write your code here */
+//	/* For example: for(;;) { } */
+	BNO085 IMU;
+	New_BNO085(&IMU, BNO080_DEFAULT_ADDRESS);
+	if(IMU.begin(&IMU.sensor) == false) {
+		while(1)
+			;
+	}
+
+	IMU.enableRotationVector(&IMU.sensor, 50);
+
+	for(;;) {
+		if(IMU.dataAvailable(&IMU.sensor)) {
+			 float roll = (IMU.getRoll(&IMU.sensor)) * 180.0 / 3.14159; // Convert roll to degrees
+			 float pitch = (IMU.getPitch(&IMU.sensor)) * 180.0 / 3.14159; // Convert pitch to degrees
+			 float yaw = (IMU.getYaw(&IMU.sensor)) * 180.0 / 3.14159; // Convert yaw to degrees
+			 for(uint8_t i = 0; i < 1; i++) {
+				 ;
+			 }
+		}
+	}
+	/*** Don't write any code pass this line, or it will be deleted during code generation. ***/
+  /*** RTOS startup code. Macro PEX_RTOS_START is defined by the RTOS component. DON'T MODIFY THIS CODE!!! ***/
+  #ifdef PEX_RTOS_START
+    PEX_RTOS_START();                  /* Startup of the selected RTOS. Macro is defined by the RTOS component. */
+  #endif
+  /*** End of RTOS startup code.  ***/
+  /*** Processor Expert end of main routine. DON'T MODIFY THIS CODE!!! ***/
+  for(;;){}
+  /*** Processor Expert end of main routine. DON'T WRITE CODE BELOW!!! ***/
+} /*** End of main routine. DO NOT MODIFY THIS TEXT!!! ***/
+
+/* END main */
+/*!
+ ** @}
+ */
+/*
+ ** ###################################################################
+ **
+ **     This file was created by Processor Expert 10.5 [05.21]
+ **     for the Freescale Kinetis series of microcontrollers.
+ **
+ ** ###################################################################
+ */
